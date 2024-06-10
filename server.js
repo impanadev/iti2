b45d16ea28faa3de2238bb5dd3cbaf9c921f63a9
impanadev <impana1141@gmail.com>
2024-06-11 09:24:19 +1000

    .

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.get('/home', (req, res) => {
 
   
   const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
+  app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
   });
 
